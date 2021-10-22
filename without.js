7577946976b1f9db751967a21221cbf0d5efe304
lenilsamuel <lenilvarghese@gmail.com>
2021-10-21 18:31:40 -0600

    final commit

---
+++ without.js
@@ -0,0 +1,32 @@
+const without = function(arr1, arr2) {
+  let arr1Obj = {};
+  let arr2Obj = {};
+  let result = [];
+  for (let i = 0; i < arr1.length; i++) arr1Obj[arr1[i]] = arr1[i];
+  for (let i = 0; i < arr2.length; i++) arr2Obj[arr2[i]] = arr2[i];
+  for (let key in arr1Obj) {
+    if (arr1Obj[key] !== arr2Obj[key]) {
+      result.push(key);
+    }
+  }
+  return console.log(result);
+  
+};
+
+const assertArrayEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
+  else console.log("🔴🔴🔴 These arrays are not equal");
+};
+
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+without(words,["lighthouse"]);
+assertArrayEqual(words, ["hello", "world", "lighthouse"]);
