0b95780822db5e44e05d777bed43ea85d71ad974
lenilsamuel <lenilvarghese@gmail.com>
2021-10-23 17:57:49 -0600

    gives iletter indices of a given string

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = function(str) {
+  let result = {};
+  for (let i = 0; i < str.length; i++) {
+    if (result[str[i]]) {
+      result[str[i]].push(i);
+    } else {
+      result[str[i]] = [i];
+    }
+    
+  }
+  return console.log(result);
+};
+
+letterPositions('lighthouse');
