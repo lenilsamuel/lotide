63962b3526472fb44b78a126252dc236a72eea2e
lenilsamuel <lenilvarghese@gmail.com>
2021-10-24 18:18:06 -0600

    map method from scratch

---
+++ map.js
@@ -0,0 +1,27 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArrayEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
+  else console.log("🔴🔴🔴 These arrays are not equal");
+};
+
+assertArrayEqual(results1, ['g', 'c', 't', 'm', 't']);
