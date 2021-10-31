1766fcad8d050ad002acf1a98487a02abb2f8127
lenilsamuel <lenilvarghese@gmail.com>
2021-10-31 15:54:15 -0600

    refactored using module.exports

--- middle.js
+++ middle.js
@@ -1,16 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-const assertArrayEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
-  else console.log("🔴🔴🔴 These arrays are not equal");
-};
-
-const middle = function(arr) {
+const middle = function (arr) {
   let result = [];
   let mid = Math.floor((arr[arr.length - 1] - arr[0]) / 2);
   if (arr.length === 1 || arr.length === 2) return result;
@@ -22,4 +10,4 @@ const middle = function(arr) {
   return result;
 };
 
-assertArrayEqual([3], middle([1, 2, 3, 4, 5]));
+module.exports = middle;
