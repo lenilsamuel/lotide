4028f1e1701a0c7fad450fc4b65ca38da8ade415
lenilsamuel <lenilvarghese@gmail.com>
2021-10-31 15:23:04 -0600

    test code only

---
+++ test/eqArraysTest.js
@@ -0,0 +1,10 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+assertEqual(eqArrays([3, 3, 1], [3, 3, 1]), true);
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
