c38b398907360a61134400ce00c65f98fead1fb0
lenilsamuel <lenilvarghese@gmail.com>
2021-10-31 14:26:36 -0600

    refactored using module.exports

--- head.js
+++ head.js
@@ -1,13 +1,6 @@
-const head = function(arr) {
+const head = function (arr) {
   if (!arr) return undefined;
   return arr[0];
 };
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  else return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
