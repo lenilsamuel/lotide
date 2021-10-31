bd74d25c6a16b40ba327c14bbc2410b74816ebaf
lenilsamuel <lenilvarghese@gmail.com>
2021-10-31 14:44:23 -0600

    refactored to use module.exports

--- tail.js
+++ tail.js
@@ -3,16 +3,11 @@ const tail = function(arr) {
   for (let item of arr) {
     tailArray.push(item);
   }
-  return tailArray.slice(1);
+  return tailArray[tailArray.length-1];
 };
 
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  else return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = tail;
+
