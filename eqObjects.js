f4d11f9e62913142cdc98108664a9e724f51a9e4
lenilsamuel <lenilvarghese@gmail.com>
2021-11-11 18:51:08 -0700

    final commit

--- eqObjects.js
+++ eqObjects.js
@@ -1,6 +1,6 @@
 function eqArrays(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) return false;
+    if (arr1[i] !== arr2[i]) return false;
   }
   return true;
 }
@@ -9,14 +9,13 @@ const eqObjects = function (obj1, obj2) {
   if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
   for (let key in obj1) {
     if (Array.isArray(obj1[key])) {
-      if(!eqArrays(obj1[key], obj2[key])) return false;
-    }
-    else if (obj1[key] !== obj2[key]) return false;
+      if (!eqArrays(obj1[key], obj2[key])) return false;
+    } else if (obj1[key] !== obj2[key]) return false;
   }
   return true;
 };
 
-const ab = { a: "1", b: "2"};
+const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 console.log(eqObjects(ab, ba)); // => true
 const abc = { a: "1", b: "2", c: "3" };
