a795d4441fc4700e2d9a2f72f076cf4937b4a423
lenilsamuel <lenilvarghese@gmail.com>
2021-11-11 18:54:38 -0700

    final commit

--- map.js
+++ map.js
@@ -1,17 +1,16 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function(array, callback) {
+const map = function (array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
+const results1 = map(words, (word) => word[0]);
 
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
@@ -19,9 +18,9 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const assertArrayEqual = function(arr1, arr2) {
+const assertArrayEqual = function (arr1, arr2) {
   if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
   else console.log("🔴🔴🔴 These arrays are not equal");
 };
 
-assertArrayEqual(results1, ['g', 'c', 't', 'm', 't']);
+assertArrayEqual(results1, ["g", "c", "t", "m", "t"]);
