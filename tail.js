a78e9cd9c8f0d60cae50f58cb8d621c18585c521
lenilsamuel <lenilvarghese@gmail.com>
2021-11-11 18:54:20 -0700

    final commit

--- tail.js
+++ tail.js
@@ -1,13 +1,9 @@
-const tail = function(arr) {
+const tail = function (arr) {
   let tailArray = [];
   for (let item of arr) {
     tailArray.push(item);
   }
-  return tailArray[tailArray.length-1];
+  return tailArray[tailArray.length - 1];
 };
 
-
-
-
 module.exports = tail;
-
