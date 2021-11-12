351b6e52a50b0a48cf64ab1934294616d450f5e4
lenilsamuel <lenilvarghese@gmail.com>
2021-11-11 18:49:22 -0700

    final commit

--- flatten.js
+++ flatten.js
@@ -1,16 +1,4 @@
-const assertArrayEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
-  else console.log("🔴🔴🔴 These arrays are not equal");
-};
-
-function eqArrays(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if(arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-}
-
-const flatten = function(arr1) {
+const flatten = function (arr1) {
   let result = [];
   for (let arr of arr1) {
     if (Array.isArray(arr)) {
@@ -22,7 +10,15 @@ const flatten = function(arr1) {
   return console.log(result);
 };
 
+const assertArrayEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
+  else console.log("🔴🔴🔴 These arrays are not equal");
+};
+
+function eqArrays(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
 module.exports = flatten;
-
-
-
