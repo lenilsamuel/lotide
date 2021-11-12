ee9ee5e8506386b4533b0b8e840dde3a9d77e944
lenilsamuel <lenilvarghese@gmail.com>
2021-11-11 18:53:15 -0700

    final commit

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,7 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected)
     return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   else return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
 };
-
-
 
 module.exports = assertEqual;
