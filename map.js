04220c62cfb6169ba20a2d94964c998c6dd694ce
lenilsamuel <lenilvarghese@gmail.com>
2021-11-14 13:23:49 -0700

    refactored to remove unnecessary spaces

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function (array, callback) {
+const map = function(array, callback) {
   const results = [];
   for (let item of array) {
     results.push(callback(item));
@@ -10,7 +10,7 @@ const map = function (array, callback) {
 
 const results1 = map(words, (word) => word[0]);
 
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
@@ -18,7 +18,7 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-const assertArrayEqual = function (arr1, arr2) {
+const assertArrayEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
   else console.log("🔴🔴🔴 These arrays are not equal");
 };
