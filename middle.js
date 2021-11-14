c075208c71f97e0f53c8c726f8b9e69000fdc201
lenilsamuel <lenilvarghese@gmail.com>
2021-11-14 13:31:26 -0700

    refactored to remove unnecessary console.log statement

--- middle.js
+++ middle.js
@@ -1,7 +1,6 @@
 const middle = function(arr) {
   let result = [];
   let mid = Math.floor([arr.length - 1] / 2);
-  console.log(mid);
   if (arr.length === 1 || arr.length === 2) return result;
   if (arr.length % 2 !== 0) result.push(arr[mid]);
   else if (arr.length % 2 === 0) {
