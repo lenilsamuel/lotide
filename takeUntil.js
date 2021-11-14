c29e557d41eac979ec4ab034f8082407bda5b7f6
lenilsamuel <lenilvarghese@gmail.com>
2021-11-14 13:22:26 -0700

    cleaned up the code to remove unnecessary spaces

--- takeUntil.js
+++ takeUntil.js
@@ -1,4 +1,4 @@
-const takeUntil = function (array, callback) {
+const takeUntil = function(array, callback) {
   let result = [];
   for (let val of array) {
     if (callback(val)) break;
@@ -23,7 +23,7 @@ const data2 = [
 ];
 const results2 = takeUntil(data2, (x) => x === ",");
 
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
@@ -31,7 +31,7 @@ const eqArrays = function (arr1, arr2) {
   return true;
 };
 
-const assertArrayEqual = function (arr1, arr2) {
+const assertArrayEqual = function(arr1, arr2) {
   if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
   else console.log("🔴🔴🔴 These arrays are not equal");
 };
