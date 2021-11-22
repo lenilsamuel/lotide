3817d203f3069788f66e6f52e2b26826fe1fed40
lenilsamuel <lenilvarghese@gmail.com>
2021-11-21 18:23:06 -0700

    fix linting issues

--- letterPositions.js
+++ letterPositions.js
@@ -1,4 +1,4 @@
-const letterPositions = function(str) {
+const letterPositions = function (str) {
   let result = {};
   for (let i = 0; i < str.length; i++) {
     if (result[str[i]]) {
@@ -6,7 +6,6 @@ const letterPositions = function(str) {
     } else {
       result[str[i]] = [i];
     }
-    
   }
   return result;
 };
