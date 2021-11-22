1d2d9c560e483fd766d306ae52d826b689ec873c
lenilsamuel <lenilvarghese@gmail.com>
2021-11-21 18:42:02 -0700

    export findKey

--- findKey.js
+++ findKey.js
@@ -4,22 +4,4 @@ const findKey = function(obj, callback) {
   }
 };
 
-let val = findKey(
-  {
-    "Blue Hill": { stars: 1 },
-    Akaleri: { stars: 3 },
-    noma: { stars: 2 },
-    elBulli: { stars: 3 },
-    Ora: { stars: 2 },
-    Akelarre: { stars: 3 },
-  },
-  (x) => x.stars === 2
-); // => "noma"
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  else return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-assertEqual(val, "noma");
+module.exports = findKey;
