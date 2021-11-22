bfe3456ac2ef6cc737a775bfc9239ebdaa4b7dac
lenilsamuel <lenilvarghese@gmail.com>
2021-11-21 18:43:41 -0700

    remove unnecessary test functions

--- flatten.js
+++ flatten.js
@@ -10,15 +10,4 @@ const flatten = function(arr1) {
   return result;
 };
 
-const assertArrayEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
-  else console.log("🔴🔴🔴 These arrays are not equal");
-};
-
-function eqArrays(arr1, arr2) {
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-}
 module.exports = flatten;
