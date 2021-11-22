1a2a620028084f14ebf2efd237f3a691b4b6efb8
lenilsamuel <lenilvarghese@gmail.com>
2021-11-21 18:49:35 -0700

    remove unnecessary test cases and export takeUntil.js

--- takeUntil.js
+++ takeUntil.js
@@ -7,34 +7,4 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, (x) => x < 0);
-
-const data2 = [
-  "I've",
-  "been",
-  "to",
-  "Hollywood",
-  ",",
-  "I've",
-  "been",
-  "to",
-  "Redwood",
-];
-const results2 = takeUntil(data2, (x) => x === ",");
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-const assertArrayEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) console.log("✅✅✅ These arrays are equal");
-  else console.log("🔴🔴🔴 These arrays are not equal");
-};
-
-assertArrayEqual(results1, [1, 2, 5, 7, 2]);
-assertArrayEqual(results2, ["I've", "been", "to", "Hollywood"]);
+module.exports = takeUntil;
