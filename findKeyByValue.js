537e3e915ef74d71933c25f135c1906922ce7bb9
lenilsamuel <lenilvarghese@gmail.com>
2021-11-21 18:42:51 -0700

    export findKeyByValue

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,9 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected)
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  else return console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const findKeyByValue = function(obj, val) {
   for (let key in obj) {
     if (obj[key] === val) return key;
@@ -11,11 +5,4 @@ const findKeyByValue = function(obj, val) {
   return undefined;
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama: "The Wire",
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
